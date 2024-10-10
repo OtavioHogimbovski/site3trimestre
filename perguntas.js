fb351a16ccc4d056ca435b6c739275df6232339b
OtavioHogimbovski <159459793+OtavioHogimbovski@users.noreply.github.com>
2024-10-10 11:32:27 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Geografia',
+    'Qual a capital da Indonesia?',
+    'Jakarta, com 11,35 milhões de habitantes.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Suécia',
+    'Estocolmo, com 980 mil habitantes.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Uruguai?',
+    'Montevidéu, com 1,38 milhões de habitantes.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Austrália?',
+    'Camberra, com 400 mil habitantes.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Canadá?',
+    'Ottawa, com 1 milhão de habitantes.'
+)
